c664bb65dfc0435a0270cc48b938e94f84931e62
rudyt7 <gallacticsoccer@gmail.com>
2020-07-27 20:43:05 +0530

    Updates

---
+++ precache-manifest.f4f1776133da5b85b42479f17951d5f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d9b36dab6a2827a3b55c8e12839f2b",
+    "url": "/chatroom-prototype/index.html"
+  },
+  {
+    "revision": "870f28e3e370f0bdb04f",
+    "url": "/chatroom-prototype/static/css/main.9e79102a.chunk.css"
+  },
+  {
+    "revision": "cdc5d023ac1885dd040b",
+    "url": "/chatroom-prototype/static/js/2.55e6a0e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chatroom-prototype/static/js/2.55e6a0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870f28e3e370f0bdb04f",
+    "url": "/chatroom-prototype/static/js/main.2828064e.chunk.js"
+  },
+  {
+    "revision": "3424a88f35d2cf14bcec",
+    "url": "/chatroom-prototype/static/js/runtime-main.9bd241c7.js"
+  }
+]);
